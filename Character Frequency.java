6e24a476b4190e130d5a899e19fa6632f8d91fae
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-03-16 22:40:42 +0530

    Add Character Frequency program in Java

---
+++ Character Frequency.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+class CharacterFrequency{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        for(int i=0;i<s.length();i++){
+
+            int count=0;
+
+            for(int j=0;j<s.length();j++){
+
+                if(s.charAt(i)==s.charAt(j)){
+                    count++;
+                }
+
+            }
+
+            System.out.println(s.charAt(i)+" = "+count);
+        }
+    }
+}
