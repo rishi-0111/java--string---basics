14076478a48b51f6430ac5ab2cf451033179d087
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-03-16 22:33:59 +0530

    Create PrintCharacters.java to print string characters
    Add a Java program to print characters of a string one by one.

---
+++ Print Characters One by One.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class PrintCharacters{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        for(int i=0;i<s.length();i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
