8558c2ed95949a4bae6f24f9bc1f4469be096254
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-08-03 11:37:56 +0530

    Add palindrome check to Reverse String program

--- Reverse String.java
+++ Reverse String.java
@@ -19,4 +19,34 @@ class ReverseString{
         System.out.println(sb.reverse().toString());
     }
 }
-
+-----------------------------
+ import java.util.*;
+class Example
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int left=0;
+        int n=s.length();
+        boolean p=true;
+        int right=n-1;
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                p=false;
+            }
+            left++;
+            right--;
+        }
+        if(p)
+        {
+            System.out.print("Palindrome");
+        }
+        else{
+            System.out.print("Not Palindrome");
+        }
+        
+    }
+}
